d26a664048ca78f4ed5305b38f2de98f30292647
Yurii M <milieninyv@gmail.com>
2025-11-12 23:31:05 +0200

    Fix: Add basename to BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,11 @@ import './index.css';
 import { ThemeProvider } from './context/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+<React.StrictMode>
+  <ThemeProvider>
+    <BrowserRouter basename="/memorylife.ee/">
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
+</React.StrictMode>
 );
